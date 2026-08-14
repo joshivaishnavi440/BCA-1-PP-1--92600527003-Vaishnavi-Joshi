5f4fd6d6ea90f09bd6a7ec4978ee85eb52999135
joshivaishnavi440 <joshivaishnavi440@gmail.com>
2026-08-14 11:01:37 +0530

    Add files via upload

---
+++ ASCII2.C
@@ -0,0 +1,15 @@
+//WAP which takes input of one character and display its ASCII code
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	char x;
+	clrscr();
+	printf("\n Enter any character : ");
+	scanf("%c", &x);
+
+	printf("\n Input character is : %c",x-32);
+	printf("\n ASCII code is : %d",x-32);
+	getch();
+}
